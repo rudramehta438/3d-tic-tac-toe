e03e9c481a3a647ee6cacdf071a448c5bd9e373f
rudramehta438 <rudramehta438@gmail.com>
2026-04-25 18:41:21 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,11 +14,16 @@ const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
         origin: "*",
-        methods: ["GET", "POST"]
+        methods: ["GET", "POST"],
+        credentials: true
     }
 });
 
-app.use(cors());
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
 app.use(express.json());
 
 // Routes
